f5b6626ececf66e4818f2cfa720081f43f04e8a8
Naimul <ah609122@gmail.com>
2025-06-23 18:52:54 +0600

    Add files via upload

---
+++ 2 hand.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+printf("rafin\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+printf("ok\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+
+return 0;
+}
---
+++ Ict-example-01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+   char ch;
+   ch= 'X';
+   printf("the character is %c",ch);
+
+   return 0;
+}
