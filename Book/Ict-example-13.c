0c663980b619617531aacd7f752f4e0c41f89976
rafin-edu <freeuser9067@gmail.com>
2025-08-16 22:50:59 +0600

    Updated

--- Book/Ict-example-13.c
+++ Book/Ict-example-13.c
@@ -2,11 +2,12 @@
 int main()
 {
     int i;
-    float n = n*3;
+    float n;
+    for(n=3; n<=30;  n=n+3)
     for ( i = 1; i<=10; i++)
 
     {
-       printf("3 X %d = %f\n", i ,n);
+       printf("3 X %d = %.0f\n", i ,n);
     }
     
     return 0;
